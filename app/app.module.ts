b0e79b6e15cb05c91af7e8f139d5040ebb839faa
Ruddy Quiroz <ruddy.quiroz88@outlook.com>
2017-06-12 00:53:06 -0600

    create a searchBox in the landing page.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { SearchBoxComponent } from './searchBox/searchBox.component';
 
 @NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
+    imports: [ 
+        BrowserModule,
+        FormsModule 
+    ],
+    declarations: [ 
+        AppComponent,
+        SearchBoxComponent 
+    ],
     bootstrap : [ AppComponent ]
 })
 
---
+++ app/searchBox/searchBox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'search-box',
+    templateUrl: './app/searchBox/searchBox.component.html',
+    styleUrls: [ './app/searchBox/searchBox.component.css' ]
+})
+
+export class SearchBoxComponent{
+
+}
