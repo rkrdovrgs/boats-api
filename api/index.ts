20c954d07d5f939d009c131a869c31e66b9c85bb
Ricardo Vargas <rkrdo.vrgs@gmail.com>
2017-07-03 02:30:20 -0400

    updated npm scripts

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,11 @@
 import * as fs from "fs";
 
 export function configureRoutes(app) {
+    let env = process.env.NODE_ENV || 'dev',
+        ext = env === 'dev' ? '.ts' : '.js';
+
     fs.readdirSync("./api")
-        .filter(f => f !== "index.ts")
+        .filter(f => f.endsWith(ext) && !f.startsWith("index."))
         .map(f => f.replace(".ts", ""))
         .forEach(file => {
             var route = require("./" + file);
